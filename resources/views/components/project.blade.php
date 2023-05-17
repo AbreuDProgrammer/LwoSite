7c3748d4ab34d575a8313756383e38903e91b80e
Leonardo <leo.abreudp@gmail.com>
2023-05-17 17:46:37 +0100

    projects row started

---
+++ resources/views/components/project.blade.php
@@ -0,0 +1,5 @@
+<div class="grid justify-items-center gap-4 p-4 border-2 rounded">
+  <h2>Tripmaster</h2>
+  <p>A website to help you building your trip</p>
+  <a href="">Github Project</a>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,21 @@
 @extends('layout')
 @section('body')
-  <nav class="bg-sky-900 w-full h-64 grid grid-cols-2 px-4">
-    <div class="text-center p-4 italic">
+  <nav class="bg-sky-900 w-full h-80 grid grid-cols-2 px-4">
+    <div class="flex flex-col items-center justify-center p-4 italic">
       <h1 class="text-8xl">Software</h1>
       <h1 class="pl-64 text-8xl">Engineer</h1>
     </div>
-    <img src="{{asset('images/me.png')}}" alt="Me" class="object-none object-top w-full h-64">
+    <img src="{{asset('images/me.png')}}" alt="Me" class="object-none object-top w-full h-80">
   </nav>
+  <div>
+    <div class="w-full p-4 bg-slate-900 space-y-12">
+      <h1>Projects:</h1>
+      <div class="grid grid-cols-4 gap-4">
+        <x-project/>
+        <x-project/>
+        <x-project/>
+        <x-project/>
+      </div>
+    </div>
+  </div>
 @endsection
