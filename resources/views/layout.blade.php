b9ecbe16539ca95bb053667347c0cd2bb4822f8b
Leonardo <leo.abreudp@gmail.com>
2023-05-17 17:07:50 +0100

    navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,13 @@
   <title>{{$title}}</title>
 </head>
 <body>
-  @yield('body')
+  <nav class="fixed top-0 left-0 w-full h-12 bg-black text-white grid grid-cols-1 content-center px-4">
+    <div class="flex items-center justify-end text-3xl">
+      <a href="">&#x2630;</a>
+    </div>
+  </nav>
+  <main class="pt-12">
+    @yield('body')
+  </main>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,4 @@
 @extends('layout')
 @section('body')
-  <h1 class="text-red-500">Welcome</h1>
-  <h2 class="text-red-500">Welcome</h2>
-  <h3 class="text-red-500">Welcome</h3>
-  <h4 class="text-red-500">Welcome</h4>
-  <h5 class="text-red-500">Welcome</h5>
-  <h6 class="text-red-500">Welcome</h6>
-  <p class="text-red-500">Welcome</p>
+
 @endsection
