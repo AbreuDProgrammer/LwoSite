332e51a076e3e272e37e006193171344aa5c1b54
AbreuDProgrammer <leo.abreudp@gmail.com>
2023-05-18 14:17:24 +0100

    home page made

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,25 +7,25 @@
   @vite('resources/css/app.css')
   <title>{{$title}}</title>
 </head>
-<body class="h-full">
-  <nav class="fixed top-0 left-0 h-12 grid grid-cols-4 content-center w-full px-96 bg-white">
+<body class="h-full bg-black/95 text-white">
+  <nav class="fixed top-0 left-0 h-16 grid grid-cols-4 content-center w-full px-96">
     <div class="text-center">
-      <h4><a href="{{route('welcome')}}">Home</a></h4>
+      <h5><a href="{{route('welcome')}}">Home</a></h5>
     </div>
     <div class="text-center">
-      <h4><a href="{{route('about')}}">About</a></h4>
+      <h5><a href="{{route('about')}}">About</a></h5>
     </div>
     <div class="text-center">
-      <h4><a href="{{route('projects')}}">Projects</a></h4>
+      <h5><a href="{{route('projects')}}">Projects</a></h5>
     </div>
     <div class="text-center">
-      <h4><a href="{{route('projects')}}">Github</a></h4>
+      <h5><a href="{{route('projects')}}">Github</a></h5>
     </div>
   </nav>
-  <main class="pt-12">
+  <main class="pt-16">
     @yield('body')
   </main>
-  <footer class="h-12 w-full bg-black flex items-center justify-start p-4 text-white bottom-0 absolute">
+  <footer class="h-12 w-full flex items-center justify-start p-4 text-white bottom-0 absolute">
     <h6>Made by Leonardo</h6>
   </footer>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 @extends('layout')
 @section('body')
-
-  <nav class="w-full p-6 text-white">
+  <nav class="w-full px-6 text-white">
     <div class="bg-sky-900 w-full h-80 grid grid-cols-2 px-4 rounded-xl">
       <div class="flex flex-col items-center justify-center p-4 italic">
         <h1 class="text-8xl">Software</h1>
@@ -10,5 +9,12 @@
       <img src="{{asset('images/me.png')}}" alt="Me" class="object-none object-top w-full h-80">
     </div>
   </nav>
-
+  <div class="p-6">
+    <h1 class="pb-6">Main Projects:</h1>
+    <div class="grid grid-cols-4 gap-4">
+      <x-project/>
+      <x-project/>
+      <x-project/>
+    </div>
+  </div>
 @endsection
